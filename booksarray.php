1f28e7ef4d83f186934d20c097a4ec26b7d97d58
Julia <jsgoransson@gmail.com>
2023-10-28 20:00:20 +0200

    Books-array result

---
+++ booksarray.php
@@ -0,0 +1,127 @@
+<?php
+
+declare(strict_types=1);
+
+$books = [
+    [
+        'title' => "The Lord of the Rings",
+        'author' => "J.R.R. Tolkien",
+        'genre' => "Epic Fantasy",
+        'color' => "yellow",
+    ],
+    [
+        'title' => "A Song of Ice and Fire",
+        'author' => "George R.R. Martin",
+        'genre' => "Epic Fantasy",
+        'color' => "pink",
+    ],
+    [
+        'title' => "Harry Potter and the Philosopher's Stone",
+        'author' => "J.K. Rowling",
+        'genre' => "Sword and Sorcery",
+        'color' => "red",
+    ],
+    [
+        'title' => "The Name of the Wind",
+        'author' => "Patrick Rothfuss",
+        'genre' => "Urban Fantasy",
+        'color' => "orange",
+    ],
+    [
+        'title' => "The Wheel of Time",
+        'author' => "Robert Jordan",
+        'genre' => "Portal Fantasy",
+        'color' => "purple",
+    ],
+    [
+        'title' => "The Chronicles of Narnia",
+        'author' => "C.S. Lewis",
+        'genre' => "High Fantasy",
+        'color' => "yellow",
+    ],
+    [
+        'title' => "Eragon",
+        'author' => "Christopher Paolini",
+        'genre' => "High Fantasy",
+        'color' => "pink",
+    ],
+    [
+        'title' => "The Lies of Locke Lamora",
+        'author' => "Scott Lynch",
+        'genre' => "Portal Fantasy",
+        'color' => "pink",
+    ],
+    [
+        'title' => "The Way of Kings",
+        'author' => "Brandon Sanderson",
+        'genre' => "Epic Fantasy",
+        'color' => "green",
+    ],
+    [
+        'title' => "The Sword of Shannara",
+        'author' => "Terry Brooks",
+        'genre' => "Sword and Sorcery",
+        'color' => "red",
+    ],
+    [
+        'title' => "The Last Unicorn",
+        'author' => "Peter S. Beagle",
+        'genre' => "Epic Fantasy",
+        'color' => "blue",
+    ],
+    [
+        'title' => "The Magicians",
+        'author' => "Lev Grossman",
+        'genre' => "Portal Fantasy",
+        'color' => "purple",
+    ],
+    [
+        'title' => "The Dark Tower",
+        'author' => "Stephen King",
+        'genre' => "Dark Fantasy",
+        'color' => "black",
+    ],
+    [
+        'title' => "Blood of Elves",
+        'author' => "Andrzej Sapkowski",
+        'genre' => "Portal Fantasy",
+        'color' => "red",
+    ],
+    [
+        'title' => "The Name of the Dragon",
+        'author' => "Elena Silverwind",
+        'genre' => "Portal Fantasy",
+        'color' => "green",
+    ],
+    [
+        'title' => "Wizard's Quest",
+        'author' => "Gareth Blackthorn",
+        'genre' => "Urban Fantasy",
+        'color' => "blue",
+    ],
+    [
+        'title' => "The Chronicles of Eldoria",
+        'author' => "Luna Nightshade",
+        'genre' => "High Fantasy",
+        'color' => "blue",
+    ],
+    [
+        'title' => "The Lost Kingdom of Zephyria",
+        'author' => "Sorin Fireheart",
+        'genre' => "Sword and Sorcery",
+        'color' => "orange",
+    ],
+    [
+        'title' => "The Enchanted Grimoire",
+        'author' => "Isabella Moonshadow",
+        'genre' => "Epic Fantasy",
+        'color' => "black",
+    ],
+    [
+        'title' => "The Shadow Realm Chronicles",
+        'author' => "Thorne Shadowcaster",
+        'genre' => "High Fantasy",
+        'color' => "black",
+    ],
+
+];
