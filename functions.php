e6c2c56b950c53646e8c0e73c985fd37ab490476
Julia <jsgoransson@gmail.com>
2023-10-28 20:00:36 +0200

    Sorting and search functions

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__ . '/booksarray.php';
+
+//Sort/order function
+function sortBooks(array $books)
+{
+    foreach ($books as $book) : ?>
+        <div class="book book-color-<?= $book['color']; ?>">
+            <p class="book-title"><?= $book['title']; ?></p>
+            <p class="book-author"><?= $book['author']; ?></p>
+        </div>
+        <?php endforeach;
+}
+
+//Search function
+function searchBooks(array $books)
+{
+    if (isset($_POST['search']))
+
+        foreach ($books as $book) {
+            $title = strtolower($book['title']);
+            $author = strtolower($book['author']);
+            $searchQuery = strtolower($_POST['search']);
+            if (str_contains($title, $searchQuery) || str_contains($author, $searchQuery)) { ?>
+            <div class="book book-color-<?= $book['color']; ?>">
+                <p class="book-title"><?= $book['title']; ?></p>
+                <p class="book-author"><?= $book['author']; ?></p>
+            </div>
+<?php
+            }
+        }
+}
